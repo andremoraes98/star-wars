2375da87f15a58b57bc2cdac06374071bb17167c
André Luis Oliveira Rocha de Moraes <andre.moraes.98@gmail.com>
2022-04-18 17:38:44 -0300

    Cumprimento do requisito 2

--- src/Components/PlanetTable.js
+++ src/Components/PlanetTable.js
@@ -1,102 +1,118 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import ContextPlanets from '../Context/ContextPlanets';
 
 function PlanetTable() {
   const context = useContext(ContextPlanets);
+  const [nameValue, setNameValue] = useState('');
+  const nameFiltered = context.filter((planet) => planet.name.includes(nameValue));
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>
-            Name
-          </th>
-          <th>
-            Rotation Period
-          </th>
-          <th>
-            Orbital Period
-          </th>
-          <th>
-            Diameter
-          </th>
-          <th>
-            Climate
-          </th>
-          <th>
-            Gravity
-          </th>
-          <th>
-            Terrain
-          </th>
-          <th>
-            Surface Water
-          </th>
-          <th>
-            Population
-          </th>
-          <th>
-            Films
-          </th>
-          <th>
-            Created
-          </th>
-          <th>
-            Edited
-          </th>
-          <th>
-            URL
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        {
-          context.map((planet, index) => (
-            <tr key={ index }>
-              <td>
-                { planet.name }
-              </td>
-              <td>
-                { planet.rotation_period }
-              </td>
-              <td>
-                { planet.orbital_period }
-              </td>
-              <td>
-                { planet.diameter }
-              </td>
-              <td>
-                { planet.climate }
-              </td>
-              <td>
-                { planet.gravity }
-              </td>
-              <td>
-                { planet.terrain }
-              </td>
-              <td>
-                { planet.surface_water }
-              </td>
-              <td>
-                { planet.population }
-              </td>
-              <td>
-                { planet.films }
-              </td>
-              <td>
-                { planet.created }
-              </td>
-              <td>
-                { planet.edited }
-              </td>
-              <td>
-                { planet.url }
-              </td>
-            </tr>
-          ))
-        }
-      </tbody>
-    </table>
+    <>
+      <label htmlFor="name-filter">
+        Name:
+        <input
+          type="text"
+          data-testid="name-filter"
+          id="name-filter"
+          value={ nameValue }
+          onChange={ ({ target }) => {
+            setNameValue(target.value);
+          } }
+        />
+      </label>
+      <table>
+        <thead>
+          <tr>
+            <th>
+              Name
+            </th>
+            <th>
+              Rotation Period
+            </th>
+            <th>
+              Orbital Period
+            </th>
+            <th>
+              Diameter
+            </th>
+            <th>
+              Climate
+            </th>
+            <th>
+              Gravity
+            </th>
+            <th>
+              Terrain
+            </th>
+            <th>
+              Surface Water
+            </th>
+            <th>
+              Population
+            </th>
+            <th>
+              Films
+            </th>
+            <th>
+              Created
+            </th>
+            <th>
+              Edited
+            </th>
+            <th>
+              URL
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            nameFiltered.map((planet, index) => (
+              <tr key={ index }>
+                <td>
+                  { planet.name }
+                </td>
+                <td>
+                  { planet.rotation_period }
+                </td>
+                <td>
+                  { planet.orbital_period }
+                </td>
+                <td>
+                  { planet.diameter }
+                </td>
+                <td>
+                  { planet.climate }
+                </td>
+                <td>
+                  { planet.gravity }
+                </td>
+                <td>
+                  { planet.terrain }
+                </td>
+                <td>
+                  { planet.surface_water }
+                </td>
+                <td>
+                  { planet.population }
+                </td>
+                <td>
+                  { planet.films }
+                </td>
+                <td>
+                  { planet.created }
+                </td>
+                <td>
+                  { planet.edited }
+                </td>
+                <td>
+                  { planet.url }
+                </td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+    </>
   );
 }
 
