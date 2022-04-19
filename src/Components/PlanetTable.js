d8c3423fcf60f739f74d4fbab88f27791f0f02d6
André Luis Oliveira Rocha de Moraes <andre.moraes.98@gmail.com>
2022-04-19 12:18:05 -0300

    Refatoração do requisito 3

--- src/Components/PlanetTable.js
+++ src/Components/PlanetTable.js
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react';
 import ContextFilter from '../Context/ContextFilter';
 import ContextPlanets from '../Context/ContextPlanets';
+import TableHead from './TableHead';
+import TableRow from './TableRow';
 
 function PlanetTable() {
   const filterContext = useContext(ContextFilter);
@@ -32,138 +34,22 @@ function PlanetTable() {
 
   return (
     <table>
-      <thead>
-        <tr>
-          <th>
-            Name
-          </th>
-          <th>
-            Rotation Period
-          </th>
-          <th>
-            Orbital Period
-          </th>
-          <th>
-            Diameter
-          </th>
-          <th>
-            Climate
-          </th>
-          <th>
-            Gravity
-          </th>
-          <th>
-            Terrain
-          </th>
-          <th>
-            Surface Water
-          </th>
-          <th>
-            Population
-          </th>
-          <th>
-            Films
-          </th>
-          <th>
-            Created
-          </th>
-          <th>
-            Edited
-          </th>
-          <th>
-            URL
-          </th>
-        </tr>
-      </thead>
+      <TableHead />
       <tbody>
         {
           filterContext.filterByNumericValues.length === 0
             ? (filteredNamePlanets.map((planet, index) => (
-              <tr key={ index }>
-                <td>
-                  { planet.name }
-                </td>
-                <td>
-                  { planet.rotation_period }
-                </td>
-                <td>
-                  { planet.orbital_period }
-                </td>
-                <td>
-                  { planet.diameter }
-                </td>
-                <td>
-                  { planet.climate }
-                </td>
-                <td>
-                  { planet.gravity }
-                </td>
-                <td>
-                  { planet.terrain }
-                </td>
-                <td>
-                  { planet.surface_water }
-                </td>
-                <td>
-                  { planet.population }
-                </td>
-                <td>
-                  { planet.films }
-                </td>
-                <td>
-                  { planet.created }
-                </td>
-                <td>
-                  { planet.edited }
-                </td>
-                <td>
-                  { planet.url }
-                </td>
-              </tr>
+              <TableRow
+                planet={ planet }
+                key={ index }
+              />
             )))
             : (
-              filteredNamePlanets.map((planet, index) => (
-                <tr key={ index }>
-                  <td>
-                    { planet.name }
-                  </td>
-                  <td>
-                    { planet.rotation_period }
-                  </td>
-                  <td>
-                    { planet.orbital_period }
-                  </td>
-                  <td>
-                    { planet.diameter }
-                  </td>
-                  <td>
-                    { planet.climate }
-                  </td>
-                  <td>
-                    { planet.gravity }
-                  </td>
-                  <td>
-                    { planet.terrain }
-                  </td>
-                  <td>
-                    { planet.surface_water }
-                  </td>
-                  <td>
-                    { planet.population }
-                  </td>
-                  <td>
-                    { planet.films }
-                  </td>
-                  <td>
-                    { planet.created }
-                  </td>
-                  <td>
-                    { planet.edited }
-                  </td>
-                  <td>
-                    { planet.url }
-                  </td>
-                </tr>
+              filteredNamePlanets.map((planets, index) => (
+                <TableRow
+                  planet={ planets }
+                  key={ index }
+                />
               ))
             )
         }
